1eb588560cd178e4c0763d906e1c18da41805f7b
Atanásio LF <70675743+Atanasildo@users.noreply.github.com>
2026-05-25 07:41:22 +0100

    Update MainActivity.kt

--- app/src/main/java/ao/elephantbet/aviatorbot/MainActivity.kt
+++ app/src/main/java/ao/elephantbet/aviatorbot/MainActivity.kt
@@ -412,7 +412,7 @@ class MainActivity : AppCompatActivity() {
     private val SUPA_URL = "https://oulidkbxjfrddluoqsif.supabase.co"
     private val SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91bGlka2J4amZyZGRsdW9xc2lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NjU5OTEsImV4cCI6MjA5NDU0MTk5MX0.y1Bjum06WIQ0meZlOoOQrzCj8xTRXYTlDEHxTccWFFA"
     private val TABELA = "credenciais"
-    private val VERSAO_ATUAL = "3.4"
+    private val VERSAO_ATUAL = "3.5"
 
     private val GROQ_KEY  = "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" // ⚠ ACTUALIZAR SE 401
     private val GROQ_URL  = "https://api.groq.com/openai/v1/chat/completions"
