7e612ed7e9eb4ae4005c5f57e29f826910debd49
Atanásio LF <70675743+Atanasildo@users.noreply.github.com>
2026-06-03 01:25:05 +0100

    Update MainActivity.kt

--- app/src/main/java/ao/elephantbet/aviatorbot/MainActivity.kt
+++ app/src/main/java/ao/elephantbet/aviatorbot/MainActivity.kt
@@ -474,7 +474,7 @@ class MainActivity : AppCompatActivity() {
     private val SUPA_URL = "https://oulidkbxjfrddluoqsif.supabase.co"
     private val SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91bGlka2J4amZyZGRsdW9xc2lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NjU5OTEsImV4cCI6MjA5NDU0MTk5MX0.y1Bjum06WIQ0meZlOoOQrzCj8xTRXYTlDEHxTccWFFA"
     private val TABELA = "credenciais"
-    private val VERSAO_ATUAL = "3.6"
+    private val VERSAO_ATUAL = "3.9"
 
     // OpenRouter — 1.º provedor principal (chave 1 e chave 2 fallback)
     private val OR_KEY   = "sk-or-v1-cbdb43d2442f14b7" + "00691bb6e4cf3493fcc0fe0c5ee3d4dbd0d2a0ac4cf201ea"
